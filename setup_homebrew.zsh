ff2e4236c062d77abb2a9c10ad8497f797c27bd9
Brian Fung <70641699+fungiwnl@users.noreply.github.com>
2021-10-21 17:58:24 +1100

    Add setup homebrew to install config

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,5 +1,14 @@
 #!/usr/bin/env zsh
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "\n<<< Starting Homebrew Setup >>>\n"
 
+if [ -x "$(command -v brew)" ]
+then
+  echo "\n<<< Brew exists, skipping install >>>\n"
+else
+  echo "\n<<< Brew doesn't exist, continuing with install >>>\n"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo "\n<<< Running Brew bundle >>>\n"
 brew bundle --verbose
