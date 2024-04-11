cef6858d0321243f144203d554349f77c6c85b6b
Brian Fung <70641699+fungiwnl@users.noreply.github.com>
2024-04-11 12:47:58 +1000

    feat: check in zshrc dotfiles

---
+++ zshrc/general.sh
@@ -0,0 +1,5 @@
+eval "$(fnm env --use-on-cd)"
+
+alias ls="eza -lfah"
+alias vi="nvim"
+alias vim="nvim"
---
+++ zshrc/init.sh
@@ -0,0 +1,5 @@
+source ~/powerlevel10k/powerlevel10k.zsh-theme
+export ZSH=$HOME/.oh-my-zsh
+source $ZSH/oh-my-zsh.sh
+
+export PATH="/usr/local/sbin:$PATH"
